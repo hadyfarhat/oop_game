1ee6e137ad3c90a71cf0b5939199d7130615cbe1
Hadi <hadyfarhat97@gmail.com>
2020-08-23 23:45:58 +0100

    add phrase to display

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,29 @@
-/* Treehouse FSJS Techdegree
- * Project 4 - OOP Game App
- * Phrase.js */
+class Phrase {
+    /**
+     * Creates a phrase
+     * @param {string} phrase 
+     */
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+
+    /**
+     * Adds letter placeholders to the display
+     */
+    addPhraseToDisplay() {
+        const phraseLettersList = document.querySelector('div#phrase ul');
+        
+        for (let i = 0; i < this.phrase.length; ++i) {
+            let li = document.createElement('li');
+            li.textContent = this.phrase[i];
+            
+            if (this.phrase[i] === " ") {
+                li.classList.add('space');
+            } else {
+                li.classList.add("hide", "letter", this.phrase[i]);
+            }
+            
+            phraseLettersList.appendChild(li);
+        }
+    }
+}
