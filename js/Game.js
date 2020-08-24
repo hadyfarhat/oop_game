3dc6f37f869a69a2444a1398498fb596afa71fb0
Hadi <hadyfarhat97@gmail.com>
2020-08-24 20:32:19 +0100

    Game

---
+++ js/Game.js
@@ -0,0 +1,115 @@
+class Game {
+    constructor() {
+        this.missed = 0;
+        this.phrases = this.createPhrases();
+        this.activePhrase = null;
+    }
+
+    /**
+     * Creates 5 phrases
+     * @return {Phrase[]} array of Phrases
+     */
+    createPhrases() {
+        let phrases = [
+            "Absobloodylutely",
+            "Christ on a bike",
+            "Bloody hell",
+            "Blimey",
+            "Splendid",
+        ];
+
+        let phrasesObjects = [];
+
+        for (let i = 0; i < phrases.length; ++i) {
+            phrasesObjects.push(new Phrase(phrases[i]));
+        }
+
+        return phrasesObjects;
+    }
+
+    /**
+     * Randomly retrieves one of the phrases stored in the phrases array
+     */
+    getRandomPhrase() {
+        const random = Math.floor(Math.random() * 5);
+        return this.phrases[random];
+    }
+
+    /**
+     * Hide overlay, choose random phrase and display it on board
+     */
+    startGame() {
+        document.querySelector("#overlay").style.display = "none";
+        this.activePhrase = this.getRandomPhrase();
+        this.activePhrase.addPhraseToDisplay();
+    }
+
+    /**
+     * Checks to see if the player has revealed all of the letters
+     * in the active phrase.
+     * @return {boolean}
+     */
+    checkForWin() {
+        const phraseLettersList = document.querySelectorAll(
+            "div#phrase li.letter"
+        );
+
+        for (let i = 0; i < phraseLettersList.length; ++i) {
+            if (phraseLettersList[i].classList.contains('hide')) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * - Displays the original start screen overlay
+     * - Updates the overlay h1 element with a friendly win or loss message
+     * depending on the outcome.
+     * @param {boolean} - if player has won or lost
+     */
+    gameOver(outcome) {
+        const overlay = document.querySelector("#overlay");
+        overlay.style.display = "";
+        const h1 = document.querySelector('h1#game-over-message');
+
+        if (outcome) {
+            h1.textContent = "You won!";
+            overlay.classList.replace("start", "win");
+        } else {
+            h1.textContent = "You lost!";
+            overlay.classList.replace("start", "lose");
+        }
+    }
+
+    /**
+     * - Removes a life from the scoreboard, by replacing one of
+     * the liveHeart.png images with a lostHeart.png image
+     * - if player missed 5 times, call game over
+     */
+    removeLife() {
+        const hearts = document.querySelectorAll('#scoreboard img');
+        hearts[this.missed].setAttribute('src', 'images/lostHeart.png');
+        this.missed += 1;
+
+        if (this.missed == 5) this.gameOver(false);
+    }
+
+    /**
+     * Handles the game's logic.
+     * @param {button HTML element} letter - letter being clicked
+     */
+    handleInteraction(letter) {
+        if (this.activePhrase.checkLetter(letter.textContent)) {
+            letter.classList.add('chosen');
+            this.activePhrase.showMatchedLetters(letter.textContent);
+            if (this.checkForWin()) {
+                this.gameOver(true);
+            }
+        } else {
+            letter.classList.add('wrong');
+            this.removeLife();
+        }
+    }
+}
--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
-document.querySelector('#overlay').style.display = 'none';
-const phrase = new Phrase("Hello world");
-phrase.addPhraseToDisplay();
-phrase.showMatchedLetters('o');
+
+const game = new Game();
+
+document.querySelector('#btn__reset').addEventListener('click', () => {
+    game.startGame();
+    document.querySelector('#qwerty').addEventListener('click', e => {
+        if (e.target.tagName == 'BUTTON') {
+            game.handleInteraction(e.target);
+        }
+    });
+});
